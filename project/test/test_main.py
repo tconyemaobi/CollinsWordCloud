e8381573f3d9bc3b5547265d8390647a856b0775
Collins Onyemaobi <tc.onyemaobi@gmail.com>
2022-06-02 23:23:41 +0100

    updated

--- project/test/test_main.py
+++ project/test/test_main.py
@@ -3,6 +3,7 @@ from main import app
 
 client = TestClient(app)
 
-def test_main_resource():
-    response_auth = client.get("/")
-    assert response_auth.status_code == 200
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
