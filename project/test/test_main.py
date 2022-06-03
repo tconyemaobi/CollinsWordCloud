70d1aa759c4ab5349dd022131403404cd6c8df04
Collins Onyemaobi <tc.onyemaobi@gmail.com>
2022-06-03 15:17:32 +0100

    reserved back

--- project/test/test_main.py
+++ project/test/test_main.py
@@ -3,7 +3,11 @@ from main import app
 
 client = TestClient(app)
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+def test_main_resource():
+    response_auth = client.get("/")
+    assert response_auth.status_code == 200
+
+# def test_root():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Hello World"}
